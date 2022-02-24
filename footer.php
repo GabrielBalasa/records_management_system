167e0e1bf289bf2a734022acf7ada1ace7961205
Gabriel <ionutgbalasa@gmail.com>
2022-02-24 17:23:57 +0000

    full-project

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Woodlands University College 2021</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
